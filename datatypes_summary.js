d7a93146e8c0c64a4f51fd1cd46f1a8829a4e3d4
MD Naimure Rahaman Emon <mdnaimurerahamanemon@gmail.com>
2025-09-29 22:47:27 +0600

    Stack & Heap

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -1,56 +1,79 @@
-//  Primitive
+// //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+// //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
-
-
-
-// Reference (Non primitive)
-
-// Array, Objects, Functions
-
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
-
-const myFunction = function(){
-    console.log("Hello world");
-}
-
-console.log(typeof anotherId);
-
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+// // const bigNumber = 3456543576654356754n
 
 
-//NOTE:
 
-// the typeof in primitive data types:-
 
-// 1. String => string
-// 2. Number => number
-// 3. Boolean => boolean
-// 4. Symbol  => symbol
-// 5. Undefined =>undefined
-// 6. BigInt => bigint
-// 7. Null => object
+// // Reference (Non primitive)
 
-// The typeof in non-primitive(Reference) data-types:-
+// // Array, Objects, Functions
 
-// 1. function() => function
-// 2. Obect => object
-// 3. Array => object
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// //NOTE:
+
+// // the typeof in primitive data types:-
+
+// // 1. String => string
+// // 2. Number => number
+// // 3. Boolean => boolean
+// // 4. Symbol  => symbol
+// // 5. Undefined =>undefined
+// // 6. BigInt => bigint
+// // 7. Null => object
+
+// // The typeof in non-primitive(Reference) data-types:-
+
+// // 1. function() => function
+// // 2. Obect => object
+// // 3. Array => object
+
+
+//**********************STACK & HEAP*************************
+
+// let Name = "Emon"
+
+// let anotherName = "Nishi"
+
+// console.log(anotherName)
+
+// let userA = {
+//     id : 1 ,
+//     email : "emon@email.com" ,
+// }
+
+// let userB = userA
+
+// userB.email = "nishi@email.com"
+
+
+// console.log(userA)
+// console.log(userB)
