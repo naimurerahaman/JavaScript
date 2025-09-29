24874ab940b0b550686cec52d016e69bd33d8ad3
MD Naimure Rahaman Emon <mdnaimurerahamanemon@gmail.com>
2025-09-29 14:26:55 +0600

    learned advanced Conversion opertaions

--- conversionOperations.js
+++ conversionOperations.js
@@ -1,17 +1,56 @@
-// let CGPA = 3.55
+let CGPA = 3.55
 
 // console.log(typeof CGPA)
 
-// let convertToString = String(CGPA)
+let convertToString = String(CGPA)
 // console.log(convertToString)
 // console.log(typeof convertToString)
 
-// let convertToBoolean = Boolean(CGPA)
+let convertToBoolean = Boolean(CGPA)
 // console.log(convertToBoolean)
 // console.log(typeof convertToBoolean)
 
 let name = ""
 
 let convertToNumber = Number(name)
-console.log(convertToNumber)
-console.log(typeof convertToNumber)
+// console.log(convertToNumber)
+// console.log(typeof convertToNumber)
+
+
+// ****************OPERATIONS***********************
+
+// console.log(3+1+'2'+2+2)
+
+//postfix optN:
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+// console.log(x,y)
+// console.log(x2, y2)
+
+//prefix optN :
+
+// let x = 3;
+// const y = ++x;
+// // x is 4; y is 4
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// // x2 is 4n; y2 is 4n
+// console.log(x,y)
+// console.log(x2, y2)
+
+
+// let x = 1
+// x++
+// console.log(x)
+
+
+let x = 1
+
+console.log(++x)
