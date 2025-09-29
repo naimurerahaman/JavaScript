1e7d62ef92043132a4b5b5103656a14a5ecd7fbd
MD Naimure Rahaman Emon <mdnaimurerahamanemon@gmail.com>
2025-09-29 13:33:49 +0600

    learned conversion operations of data types

---
+++ conversionOperations.js
@@ -0,0 +1,17 @@
+// let CGPA = 3.55
+
+// console.log(typeof CGPA)
+
+// let convertToString = String(CGPA)
+// console.log(convertToString)
+// console.log(typeof convertToString)
+
+// let convertToBoolean = Boolean(CGPA)
+// console.log(convertToBoolean)
+// console.log(typeof convertToBoolean)
+
+let name = ""
+
+let convertToNumber = Number(name)
+console.log(convertToNumber)
+console.log(typeof convertToNumber)
