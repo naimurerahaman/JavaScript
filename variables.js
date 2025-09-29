0bed9e5925d852a325f7f034a68f06c532b3e295
MD Naimure Rahaman Emon <mdnaimurerahamanemon@gmail.com>
2025-09-29 12:41:04 +0600

    learned variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+const userID = 1
+let userName = "MD Naimure Rahaman Emon"
+var userAddress = "Khulna, Bangladesh" //prefer not to user var!
+userEmail = "emon@gmail.com" // this can be written but not prefered
+
+console.log(userName)
+
+console.table([userID,userName,userAddress,userEmail])
+
