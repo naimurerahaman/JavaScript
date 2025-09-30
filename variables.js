e1f2e47f33d17feb1d904fd5db5f77e89068b041
MD Naimure Rahaman Emon <mdnaimurerahamanemon@gmail.com>
2025-09-30 13:40:30 +0600

    ..

--- variables.js
+++ variables.js
@@ -3,7 +3,8 @@ let userName = "MD Naimure Rahaman Emon"
 var userAddress = "Khulna, Bangladesh" //prefer not to user var!
 userEmail = "emon@gmail.com" // this can be written but not prefered
 
-console.log(userName)
+console.log(userName) //to get single output
 
-console.table([userID,userName,userAddress,userEmail])
+console.table([userID,userName,userAddress,userEmail]) //to get multiple output in a single table
+
 
