f68ffc198b6978b58006f1d166ffa105b36142ce
MD Naimure Rahaman Emon <mdnaimurerahamanemon@gmail.com>
2025-10-03 12:21:43 +0600

    practised numbers and maths

---
+++ nums_and_maths.js
@@ -0,0 +1,45 @@
+// const balance = 1000000
+
+// const balance = new Number(10000000000000)
+
+// console.log(balance.toString())
+
+// console.log(balance.toFixed(5))
+
+// console.log(balance.toPrecision(1))
+
+// console.log(balance.toLocaleString('en-IN'))
+
+//**************************************************Maths************************************
+
+// console.log(Math.PI)
+
+// console.log(Math.E)
+
+// console.log(Math.abs(-100))
+
+// console.log(Math.round(4.6))
+
+// console.log(Math.ceil(4.1))
+
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(4, 1, 9, -1))
+
+// console.log(Math.max(4, 1, 9, -1))
+
+// console.log(Math.random()) //0 to 0.999999
+
+// console.log(Math.random() * 10) //0 to 9.99999
+
+// console.log(Math.floor(Math.random() * 10)) //0 to 9
+
+// console.log(Math.floor(Math.random() * 10) + 1) //1 to 10 
+
+
+//making a random number between 1 to 6(dice)
+
+const max = 6
+const min = 1
+
+console.log(Math.floor(Math.random() * (max - min +1)))
